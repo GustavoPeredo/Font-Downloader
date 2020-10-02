cea13eb70a26e62b93d23acefbe06a7d91917b2b
GustavoPeredo <gustavomperedo@protonmail.com>
2020-10-01 21:37:38 -0300

    Lot's of improvements!

--- src/window.py
+++ src/window.py
@@ -14,7 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 #Import nescessary libraries and modules
 #from gettext import gettext as _
 from gi.repository import Gdk, Gio, Gtk, Handy, GObject, WebKit2
@@ -25,17 +24,10 @@ from urllib.request import urlretrieve
 
 #Init Webkit and Handy libs
 Handy.init()
-WebKit2.WebView()
 
 locale.bindtextdomain('fontdownloader', path.join(path.dirname(__file__).split('fontdownloader')[0],'locale'))
 locale.textdomain('fontdownloader')
-#Try to update webfonts, otherwise pass
-try:
-    urlretrieve('https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyA2dEVFiF8o1q8JnSGCsq1reUAbzZR6z0I', 'webfonts.json')
-except:
-    pass
-
-webfontsData = json.load(open("webfonts.json", 'r'))
+webfontsData = json.load(open("/app/share/fontdownloader/fontdownloader/webfonts.json", 'r'))
 
 #Here we import the font-box template which is used for the fonts' boxes
 @Gtk.Template(resource_path='/org/gustavoperedo/FontDownloader/font-box.ui')
@@ -51,12 +43,16 @@ class FontBox(Gtk.Box):
         self.data = [familyName, category, variants, subset]
         #Set labels' texts
         self.fontFamily.set_text(familyName)
-        self.fontCategory.set_text(category)
+        #Change category to it's translation
+        self.fontCategory.set_text(category.replace('sans-serif',(_('sans-serif')).replace('serif',(_('serif'))).replace('display',(_('display'))).replace('monospaced',(_('monospaced'))).replace('handwriting',(_('handwriting')))))
+
 
 #Here we import the main window template
 @Gtk.Template(resource_path='/org/gustavoperedo/FontDownloader/window.ui')
 class FontdownloaderWindow(Handy.Window):
     __gtype_name__ = 'FontdownloaderWindow'
+    #Get settings schema
+    settings = Gio.Settings.new('org.gustavoperedo.FontDownloader')
 
     #And here we import everything, basically
     back_button = Gtk.Template.Child()
@@ -80,7 +76,44 @@ class FontdownloaderWindow(Handy.Window):
     about_button = Gtk.Template.Child()
     headerbar1 = Gtk.Template.Child()
     headerbar2 = Gtk.Template.Child()
+    light_mode_button = Gtk.Template.Child()
+    dark_mode_button = Gtk.Template.Child()
+    settings_button = Gtk.Template.Child()
+    SettingsWindow = Gtk.Template.Child()
+    close_settings_button = Gtk.Template.Child()
+    folder_settings_button = Gtk.Template.Child()
+    arabic_button = Gtk.Template.Child()
+    bengali_button = Gtk.Template.Child()
+    chinese_hk_button = Gtk.Template.Child()
+    chinese_SIMP_button = Gtk.Template.Child()
+    chinese_trad_button = Gtk.Template.Child()
+    cyrillic_button = Gtk.Template.Child()
+    cyrillic_ext_button = Gtk.Template.Child()
+    devanagari_button = Gtk.Template.Child()
+    greek_button = Gtk.Template.Child()
+    greek_ext_button = Gtk.Template.Child()
+    gujarati_button = Gtk.Template.Child()
+    gurmukhi_button = Gtk.Template.Child()
+    hebrew_button = Gtk.Template.Child()
+    japanese_button = Gtk.Template.Child()
+    kannada_button = Gtk.Template.Child()
+    khmer_button = Gtk.Template.Child()
+    korean_button = Gtk.Template.Child()
+    latin_button = Gtk.Template.Child()
+    latin_ext_button = Gtk.Template.Child()
+    malayalam_button = Gtk.Template.Child()
+    myanmar_button = Gtk.Template.Child()
+    oriya_button = Gtk.Template.Child()
+    sinhala_button = Gtk.Template.Child()
+    tamil_button = Gtk.Template.Child()
+    telugu_button = Gtk.Template.Child()
+    thai_button = Gtk.Template.Child()
+    tibetan_button = Gtk.Template.Child()
+    vietnamese_button = Gtk.Template.Child()
+    any_alphabet_button = Gtk.Template.Child()
+    reset_button = Gtk.Template.Child()
 
+    WebKit2.WebView()
     #On initalization do:
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -123,9 +156,47 @@ class FontdownloaderWindow(Handy.Window):
         self.mono_check.connect('toggled', self.updateFilter)
         self.search_entry.connect('changed', self.updateFilter)
         self.about_button.connect("clicked", self.on_about)
+        self.light_mode_button.connect('clicked', self.changeTheme)
+        self.dark_mode_button.connect('clicked', self.changeTheme)
+        self.settings_button.connect('clicked', self.presentSettings)
+        self.close_settings_button.connect('clicked', self.closeSettings)
+        self.folder_settings_button.connect('clicked', self.on_open)
+        self.any_alphabet_button.connect('clicked', self.anyAlphabet)
+        self.reset_button.connect('clicked', self.reset)
+
+        self.alphabet_buttons = [self.arabic_button, self.bengali_button,
+        self.chinese_hk_button, self.chinese_SIMP_button,
+        self.chinese_trad_button, self.cyrillic_button,
+        self.cyrillic_ext_button, self.devanagari_button,
+        self.greek_button, self.greek_ext_button, self.gujarati_button,
+        self.gurmukhi_button, self.hebrew_button, self.japanese_button,
+        self.kannada_button, self.khmer_button, self.korean_button,
+        self.latin_button, self.latin_ext_button,
+        self.malayalam_button, self.myanmar_button, self.oriya_button,
+        self.sinhala_button, self.tamil_button, self.telugu_button,
+        self.thai_button, self.tibetan_button, self.vietnamese_button]
+
+        self.alphabet_list = ['arabic', 'bengali', 'chinese-hongkong',
+        'chinese-simplified', 'chinese-traditional', 'cyrillic',
+        'cyrillic-ext', 'devanagari', 'greek', 'greek-ext', 'gujarati',
+        'gurmukhi', 'hebrew', 'japanese', 'kannada', 'khmer', 'korean',
+        'latin', 'latin-ext', 'malayalam', 'myanmar', 'oriya', 'sinhala',
+        'tamil', 'telugu', 'thai', 'tibetan', 'vietnamese']
+
+        self.current_alphabet_list = self.settings.get_string('current-alphabet').split(';')
+        self.any_alphabet = self.settings.get_boolean('any-alphabet')
+
+        self.folder_settings_button.set_label(self.settings.get_string('default-directory'))
+
+        for buttons in self.alphabet_buttons:
+            buttons.connect("toggled", self.updateAlphabet)
 
         #Calls fontChanged function to update first view
         self.fontChanged()
+        self.anyAlphabet()
+
+        self.dark_mode_button.set_active(self.settings.get_boolean('dark-mode'))
+        self.changeTheme()
 
         #Sets up borders
         self.setup_css();
@@ -164,7 +235,7 @@ class FontdownloaderWindow(Handy.Window):
         #This function gets the selected font's link and downloads
         #to the '.local/share/fonts' directory
         links = webfontsData['items'][self.fonts_list.get_selected_row().get_index()]['files']
-        absolutePath = path.join(path.expanduser('~'), '.local/share/fonts')
+        absolutePath = path.join(path.expanduser('~'), '.local/share/fonts') if self.settings.get_string('default-directory') == 'Default' else self.settings.get_string('default-directory')
         if not path.exists(absolutePath):
             makedirs(absolutePath)
         for key in links:
@@ -175,12 +246,26 @@ class FontdownloaderWindow(Handy.Window):
         #This function gets the selected font's link and downloads
         #to the user's download directory
         links = webfontsData['items'][self.fonts_list.get_selected_row().get_index()]['files']
-        absolutePath = path.join(path.expanduser('~'), 'Downloads')
-        if not path.exists(absolutePath):
-            makedirs(absolutePath)
-        for key in links:
-           urlretrieve(links[key], path.join(absolutePath,
-                        self.CurrentSelectedFont + " " + key + links[key][-4:]))
+
+        dialog = Gtk.FileChooserDialog("Please choose a folder", self,
+            Gtk.FileChooserAction.SELECT_FOLDER,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.OK:
+            absolutePath = dialog.get_filename()
+            if not path.exists(absolutePath):
+                makedirs(absolutePath)
+            for key in links:
+               urlretrieve(links[key], path.join(absolutePath,
+                            self.CurrentSelectedFont + " " + key + links[key][-4:]))
+
+        elif response == Gtk.ResponseType.CANCEL:
+            pass
+
+        dialog.destroy()
 
     def checkAllFilters(self, *args, **kwargs):
         #If the user select "All" on filters, check all
@@ -209,7 +294,13 @@ class FontdownloaderWindow(Handy.Window):
 
         filtered = [filters for filters in self.CurrentFilters if self.CurrentFilters[filters]]
         searchBarText = self.search_entry.get_text().lower()
-        return ((searchBarText == row.get_child().data[0][:len(searchBarText)].lower()) and (row.get_child().data[1] in filtered))
+        if not self.any_alphabet:
+            if any(i in self.current_alphabet_list for i in row.get_child().data[3]):
+                return ((searchBarText == row.get_child().data[0][:len(searchBarText)].lower()) and (row.get_child().data[1] in filtered))
+            else:
+                return False
+        else:
+            return ((searchBarText == row.get_child().data[0][:len(searchBarText)].lower()) and (row.get_child().data[1] in filtered))
 
     def fontChanged(self, *args, **kwargs):
         #Whenever the user does something that should change the font preview:
@@ -252,7 +343,7 @@ class FontdownloaderWindow(Handy.Window):
         #Load the html, set title and subtitle
         self.font_preview.load_html(self.html)
         self.headerbar2.set_title(self.CurrentSelectedFont)
-        self.headerbar2.set_subtitle(self.temp_data[1])
+        self.headerbar2.set_subtitle(self.temp_data[1].replace('sans-serif',(_('sans-serif'))).replace('serif',(_('serif'))).replace('display',(_('display'))).replace('monospaced',(_('monospaced'))).replace('handwriting',(_('handwriting'))))
 
         #If the screen is too small, change to font preview pane and show
         #the return button, otherwise, do the opposite
@@ -272,3 +363,58 @@ class FontdownloaderWindow(Handy.Window):
         self.leaflet.set_visible_child(self.box1)
         self.main_download_button.set_label(_('Download'))
         self.back_button.hide()
+
+    def changeTheme(self, *args, **kwargs):
+        if self.dark_mode_button.get_active():
+            Gtk.Settings.get_default().set_property('gtk-application-prefer-dark-theme', True)
+            self.settings.set_boolean('dark-mode', True)
+        else:
+            Gtk.Settings.get_default().set_property('gtk-application-prefer-dark-theme', False)
+            self.settings.set_boolean('dark-mode', False)
+
+    def presentSettings(self, *args, **kwargs):
+        self.SettingsWindow.show()
+
+    def closeSettings(self, *args, **kwargs):
+        self.SettingsWindow.hide()
+
+    def on_open(self, event):
+        dialog = Gtk.FileChooserDialog(_("Please choose a folder"), self,
+            Gtk.FileChooserAction.SELECT_FOLDER,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.OK:
+            file_path = dialog.get_filename()
+            self.folder_settings_button.set_label(file_path)
+            self.settings.set_string('default-directory', file_path)
+        elif response == Gtk.ResponseType.CANCEL:
+            pass
+
+        dialog.destroy()
+
+    def updateAlphabet(self, *args, **kwargs):
+        self.current_alphabet_list = []
+        for i in range(len(self.alphabet_list)):
+            if self.alphabet_buttons[i].get_active():
+                self.current_alphabet_list.append(self.alphabet_list[i])
+        self.settings.set_boolean('any-alphabet', self.any_alphabet_button.get_active())
+        self.settings.set_string('current-alphabet', ';'.join(self.current_alphabet_list))
+
+    def anyAlphabet(self, *args, **kwargs):
+        any_alphabet = self.any_alphabet_button.get_active()
+        for i in range(len(self.alphabet_list)):
+            self.alphabet_buttons[i].set_active(any_alphabet)
+        self.updateAlphabet()
+
+    def reset(self, *args, **kwargs):
+        self.settings.set_string('default-directory', 'Default')
+        self.folder_settings_button.set_label('Default')
+        self.any_alphabet_button.set_active(True)
+        self.updateAlphabet()
+
+
+
+        
